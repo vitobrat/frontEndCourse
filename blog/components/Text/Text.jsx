cdafb24768214475779bcde9ff46c1f0d3a6fded
Victor Bratko <bratko.viktor2004@gmail.com>
2022-10-31 14:41:15 +0300

    homework_3 version 1.1

--- blog/components/Text/Text.jsx
+++ blog/components/Text/Text.jsx
@@ -3,9 +3,7 @@ import css from './text.module.css'
 
 export const Text = ({className,children}) => {
   return (
-    className == "textRed"?
-    <div className={css.text}><p><text type='text' className={css.textRed}>{children}</text></p></div>:
-    <div className={css.text}><p><text type='text' className={css.textGreen}>{children}</text></p></div>
+    <div className={css.text}><p className={className == "textRed" ? css.textRed : css.textGreen}>{children}</p></div>
 )}
 
 
